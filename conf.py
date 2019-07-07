b9605af32913376488b73df5ffd5c401b89fd02b
kaito883 <takuma.kaito@gmail.com>
2019-07-07 11:00:34 +0900

    Latest source

---
+++ conf.py
@@ -0,0 +1,7 @@
+class conf():
+	SITE_ID  = 'TestSite10'
+	SITE_NAME = 'TestSite10'
+	EXPORT_FILE_SUFFIX = '_ExportedByAutomatorTestSite10'
+	CARTRIDGE_PATH = 'plugin_cartridge_merge:plugin_instorepickup:plugin_wishlists:plugin_giftregistry:lib_productlist:plugin_productcompare:plugin_sitemap:plugin_applepay:plugin_datadownload:app_storefront_base'
+	#Site information you want to copy from
+	SITE_NAME_COPY_FROM = 'RefArchGlobal'
